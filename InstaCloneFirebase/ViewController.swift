23b80ef7352a7d9190eeedcd4def471ded6cbab1
sfctl <catalbasifa@gmail.com>
2021-01-30 16:28:41 +0300

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Yasemin YEL on 30.01.2021.
+//  Copyright © 2021 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
